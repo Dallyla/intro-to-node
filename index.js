d95359938e4ca5754ffeb1c13a2e17ea803fb8a8
Dallyla Oliveira <dallyla.s.oliveira@gmail.com>
2020-08-04 20:46:24 -0300

    intro to node.js modules

---
+++ index.js
@@ -0,0 +1,11 @@
+var superheroes = require("superheroes"); //chama o pacote 
+
+var mySuperheroName = superheroes.random();  //chama o método e armazena numa variável
+
+console.log("My superhero name is: "+ mySuperheroName);  //log na variável com o resultado
+
+const supervillains = require('supervillains');
+
+var mySuperVillainName = supervillains.random();
+
+console.log("My supervllain name is: "+ mySuperVillainName);
